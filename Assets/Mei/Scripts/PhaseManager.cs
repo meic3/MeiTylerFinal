f918dec7182f0d699b2eedc3f4833e332932b9fd
meic3 <meisachi125@gmail.com>
2025-11-17 19:32:34 -0500

    timer

---
+++ Assets/Mei/Scripts/PhaseManager.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using TMPro;
+using UnityEditor.Tilemaps;
+
+public class PhaseManager : MonoBehaviour
+{
+    public class Wave
+    {
+        public int cockroachNum;
+        public int flyNum;
+        public int stinkBugNum;
+        public Wave(int cockroach, int fly, int stinkbug)
+        {
+            cockroachNum = cockroach;
+            flyNum = fly;
+            stinkBugNum = stinkbug;
+        }
+    }
+
+    public Wave wave1 = new Wave(20,0,0);
+    public int waveCount;
+    public bool isInGame;
+    public BugGenerator bugGenerator;
+
+    [SerializeField]
+    private TextMeshProUGUI timerText;
+    private float remainingTime;
+    private float setTime = 10;
+
+    void Start()
+    {
+        bugGenerator.canGenerate = false;
+        isInGame = false;
+        remainingTime = setTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (remainingTime <= 0)
+        {
+            StartWave();
+        }
+        if (!isInGame)
+        {
+        remainingTime -= Time.deltaTime;
+        }  
+        int minutes = Mathf.FloorToInt(remainingTime / 60);
+        int seconds = Mathf.FloorToInt(remainingTime % 60);
+        timerText.text = string.Format("{0:00}:{1:00}",minutes,seconds);
+    }
+
+    public void StartWave()
+    {
+        isInGame = true;
+        remainingTime = setTime;
+        bugGenerator.canGenerate = true;
+    }
+}
