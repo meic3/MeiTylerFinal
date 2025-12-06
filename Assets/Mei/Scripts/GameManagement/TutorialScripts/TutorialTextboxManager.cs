e86b2a1704702d821eca8837103a7516813dd2d8
meic3 <meisachi125@gmail.com>
2025-12-05 21:42:27 -0500

    tutorial Text system

--- Assets/Mei/Scripts/GameManagement/TutorialScripts/TutorialTextboxManager.cs
+++ Assets/Mei/Scripts/GameManagement/TutorialScripts/TutorialTextboxManager.cs
@@ -1,26 +1,58 @@
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.AI;
+using Unity.VisualScripting;
 
 public class TutorialTextboxManager : MonoBehaviour
 {
     public List<TutorialTextboxInfo> tutorialTextboxInfos;
     public int tutorialTextIndex = 0;
 
+    private int tutorialTextNum;
+    public int tutorialTextGroupIndex = 0;
+
+    [SerializeField]
+    private TMP_Text dialogueText;
+
     public bool textShow = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        tutorialTextNum = GetGroupSize();
+        UpdateTextMessage();
+    }
+    public void NextText()
+    {
+        tutorialTextIndex ++;
+
+        if (tutorialTextIndex >= tutorialTextNum)
+        {
+            NextTextGroup();
+        }
+        else
+        {
+            UpdateTextMessage();
+        }
+
+    }
+    void NextTextGroup()
+    {
+        tutorialTextNum = GetGroupSize();
+        tutorialTextGroupIndex++;
+        tutorialTextIndex = 0;
+        UpdateTextMessage();
     }
 
-    // Update is called once per frame
-    void Update()
+    void UpdateTextMessage()
     {
-        
+        dialogueText.text = tutorialTextboxInfos[tutorialTextGroupIndex].Messages[tutorialTextIndex];
     }
 
-    void NextText()
+    int GetGroupSize()
     {
-        tutorialTextIndex++;
+        int count = tutorialTextboxInfos[tutorialTextGroupIndex].Messages.Count;
+        return count;
     }
+
 }
